36eb65f66f39f29da79e2c1f672f76a88f3394a6
EmilG89 <emil.guseimnov@icloud.com>
2023-05-12 18:13:00 -0400

    lab-work partially completed

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,21 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+
+    return (
+        <div className='alert alert-secondary'>
+            <div>
+                Budget: £
+                <input 
+                    type='number'
+                    step='10'
+                    value = {budget}
+                    >
+                </input>
+            </div>
+        </div>
+    );
+};
+export default Budget;
